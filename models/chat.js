b5c5c120c3096f65c53dc446e0ae933c56cc5c2a
Juan M. de la Fuente <jmdelafuente22@gmail.com>
2020-07-24 12:27:30 -0300

    Minima estructura de chat v1

---
+++ models/chat.js
@@ -0,0 +1,9 @@
+class Chat{
+    constructor (id, origen, timestamp, lastmessage, name){
+        this.id = id;
+        this.origin = origen;
+        this.timestamp = timestamp;
+        this.lastmessage = lastmessage;
+        this.name = name;
+    }
+}
