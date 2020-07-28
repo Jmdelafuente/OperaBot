5253a58930929bec4ff1a0c9d33964d38a820ae6
Juan M. de la Fuente <jmdelafuente22@gmail.com>
2020-07-28 15:01:41 -0300

    Limpieza de APIs innecesarias

--- api.js
+++ api.js
@@ -20,32 +20,33 @@ app.use(function (req, res, next) {
 });
 
 // * Begging of API
-app.get("/api/allChats", (req, res) => {
-  // TODO authenticate origin
-  // * Get all WA chats
-  var wa_list = [];
-  var fc_list = [];
-  var chat_list = [];
-  var options = {
-    method: "POST",
-    url: "http://localhost:3003/wa/list",
-    headers: {
-      "Content-Type": "application/json",
-    },
-  };
-  request(options, function (error, response) {
-    if (error) throw new Error(error);
-    wa_list = response.body;
-  });
-  // ? Add a mark for know is whatsapp chat
-  // * Get all Facebook chats
-  // TODO consume api
+// ? Tiene sentido pedir todos los chats a la API si no es por websocket?
+  app.get("/api/allChats", (req, res) => {
+  // ? TODO authenticate origin
+  // ? // * Get all WA chats
+  // ? var wa_list = [];
+  // ? var fc_list = [];
+  // ? var chat_list = [];
+  // ? var options = {
+  // ?   method: "POST",
+  // ?   url: "http://localhost:3003/wa/list",
+  // ?   headers: {
+  // ?     "Content-Type": "application/json",
+  // ?   },
+  // ? };
+  // ? request(options, function (error, response) {
+  // ?   if (error) throw new Error(error);
+  // ?   wa_list = response.body;
+  // ? });
+  // ? // ? Add a mark for know is whatsapp chat
+  // ? // * Get all Facebook chats
+  // ? // TODO consume api
 
-  // * Order all chats by timestamp
-  // ? wa uses a variable called 't', facebook ?
+  // ? // * Order all chats by timestamp
+  // ? // ? wa uses a variable called 't', facebook ?
 
-  res.send(chat_list);
-});
+  // ? res.send(chat_list);
+}) ;
 
 // * Nuevo mensaje de whatsapp
 app.post("/api/wa/newmessage", jsonParser, (req, res) => {
@@ -66,7 +67,7 @@ app.post("/api/wa/newmessage", jsonParser, (req, res) => {
 // * Incio de WA_Server, recibimos la lista de chats
 app.post("/api/wa/list", jsonParser, (req, res) => {
   // TODO authenticate origin
-  
+  res.sendStatus(200);
 });
 
 // * Nuevo mensaje de facebook
