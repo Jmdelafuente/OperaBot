9920ef6d1a6590411e1b0e54e0814f3d48634d49
Juan M. de la Fuente <jmdelafuente22@gmail.com>
2020-07-29 15:31:56 -0300

    Nuevo evento: propagar listas de chat + mejoras en alta y baja de operador

--- websocket.js
+++ websocket.js
@@ -1,7 +1,5 @@
-module.exports.enviarMensaje = enviarMensaje;
-module.exports.recibirMensaje = recibirMensaje;
-
 // Imports from websockets
+const express = require("express");
 var appFront = express();
 var http = require("http").Server(appFront);
 var path = require("path");
@@ -25,15 +23,18 @@ io.on("connection", function (socket) {
   });
   socket.on("new_operator", function (msg) {
     // TODO: validar con weblogin el token/sessionkey
-    socket.user = msg.SESSIONKEY; // * Cambiar por nombre de usuario cuando este la conexion con WL
+    console.log(`Nuevo operador ${msg.SESSIONKEY}`);
+    socket.user = msg.SESSIONKEY; // TODO Cambiar por nombre de usuario cuando este la conexion con WL
+    op.altaOperador(msg.SESSIONKEY,socket);
     sockets[socket.id] = socket;
-    op.altaOperador(msg.SESSIONKEY); // * Cambiar por nombre de usuario cuando este la conexion con WL
-    // TODO: enviar lista de chats
   });
   // Remove disconnected op
-  socket.on("disconnect", function () {
-    op.bajaOperador(sockets[socket.id].user);
-    delete sockets[socket.id];
+  socket.on("disconnect", function (causa) {
+    if((causa == "transport close") && (sockets[socket.id])){
+      op.bajaOperador(sockets[socket.id].user);
+      delete sockets[socket.id];
+    }
+    // console.log(`Disconnect ${socket.id} ${causa}`);
   });
   // socket.on("recive_op_message", function (msg) {
   //   io.emit("recive_op_message",msg);
@@ -41,17 +42,35 @@ io.on("connection", function (socket) {
 });
 
 // Functions define for export and modularization
-const enviarMensaje = function (id, contenido){
-
-};
+const enviarMensaje = function (id, contenido) {};
 
 const recibirMensaje = function (operador, id, contenido){
     var mensaje = {};
     mensaje.id = id;
     mensaje.contenido = contenido;
     io.emit("recive_op_message", mensaje);
+    return true;
 };
+
+const asignarMensaje = function (socket, id, contenido){
+  var mensaje = {};
+  mensaje.id = id;
+  mensaje.contenido = contenido;
+  socket.emit("assign_op_message", mensaje);
+  return socket;
+}
+
+const recibirLista = function (operador, lista){
+  console.log(`Propagando lista ${lista}`);
+  operador.emit("send_op_list", lista);
+}
 
 http.listen(portFront, function () {
   console.log("Websockets on *:" + portFront);
 });
+
+
+module.exports.enviarMensaje = enviarMensaje;
+module.exports.recibirMensaje = recibirMensaje;
+module.exports.asignarMensaje = asignarMensaje;
+module.exports.recibirLista = recibirLista;
