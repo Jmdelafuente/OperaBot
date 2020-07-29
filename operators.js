8259a9da18a2f2f9d398dcd6656d249ccfcfc828
Juan M. de la Fuente <jmdelafuente22@gmail.com>
2020-07-29 15:30:42 -0300

    Desacoplada la funcionalidad, ahora solo gestiona operadores. Abstraccion de socket y mensajerias

--- operators.js
+++ operators.js
@@ -1,94 +1,28 @@
-module.exports.nuevoMensaje = nuevoMensaje;
-module.exports.enviarMensaje = enviarMensaje;
-module.exports.altaOperador = altaOperador;
-module.exports.bajaOperador = bajaOperador;
-module.exports.chat_all = chat_all;
-module.exports.operators = operators;
-
 var Queue = require("better-queue");
-const request = require("request");
-var chat_asig = {};
-var chat_all = [];
-var operators = {};
+var Chat = require("./models/Chat");
+var socket = require("./websocket");
+var messenger = require("./messengerService");
+var chat_asig = {}; // * Diccionario 'operadorID' -> socket
+var operators = {}; // * Todos los operadores disponbles
 
 // TODO: Tenemos que definir una estructura interna para el chat
 // ? posible estructura guardada en models/chat.js
-// * chat.id;           -> Posiblemente el mismo que usen F o W
-// * chat.timestamp;    -> Ultimo timestamp, puede ser local o la que traiga original
-// * chat.lastmessage;  -> Ultimo mensaje, para mantener una verificación de actualizacion
-// * chat.origin;       -> Origen 'F' o 'W'
-// * chat.name;         -> Nombre para mostrar / humano legible
-// * chat.avatar;       -> Tal vez sea necesario usar las fotos de perfil o avatares para mejor usabilidad
 
-var notify_newAssig = new Queue(function (input, cb) {
+var newAsign = new Queue(function (input, cb) {
   // Pick an op and try to assign it
-
+  // FIXME: a modo de prueba, tomamos uno 'aleatorio'
+  let op = operators[Math.floor(Math.random() * arr.length)];
+  result = socket.asignarMensaje(operators[op], input.id, input.cont);
   // Callback / response
   cb(null, result);
 });
 
-async function enviarMensaje(id, cont) {
-  // var chat = chat_all[id];
-  // ! Datos de prueba
-  var chat = {};
-  chat.origin = "W";
 
-  var org = "";
-  var port = "";
-  if ((chat.origin = "W")) {
-    org = "wa";
-    port = 3003;
-  } else {
-    org = "fa";
-    port = 3002;
-  }
-  var options = {
-    method: "POST",
-    url: `http://localhost:${port}/${org}/sendMessage`,
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      id: id,
-      message: cont,
-    }),
-  };
-  request(options, function (error, response) {
-    if (error) throw new Error(error);
-    wa_list = response.body;
-  });
-}
-
-async function nuevoMensaje(id, cont, origen) {
-  // Check if chat is already assigned
-  if (chat_asig[id]) {
-    // Push notification to operator
-    chat_asig[id].notify_newMessage(id, cont);
-  } else {
-    // * We need to assign it
-    var chat = {};
-    chat.id = id;
-    chat.origen = origen;
-    notify_newAssig
-      .push({ id: id, cont: chat })
-      .on("finish", function (res) {
-        // Save the assignment
-        chat_asig[id] = res.op;
-        return true;
-      })
-      .on("failed", function (err) {
-        // Exception, I hope never see this
-        // ? evaluar que hacer en este caso
-        return new Error("No se puedo asignar el chat");
-      });
-    chat_all.push(chat);
-  }
-}
-
-async function altaOperador(id, socket) {
-  // Create new connection for this op
-  operators[id] = socket;
+async function altaOperador(id, canal) {
   // Save {id,connection} for later
+  operators[id] = canal;
+  lista = messenger.chatsList();
+  socket.recibirLista(canal, lista);
 }
 
 async function bajaOperador(id) {
@@ -97,9 +31,9 @@ async function bajaOperador(id) {
     //  Antes de dar de baja un operador, esperamos un tiempo prudencial
     //  -4min- tal vez sea sólo una ligera desconexión.
     setTimeout(
-        (id)=>{
+        id=>{
             // Si no se volvio a conectar, le doy la desconexion logica
-            if (operators.indexOf(id) < 0) {
+            if (! operators[id]) {
                 // ? Cuando un operador se deconecta, sus chats se reasignan?
                 // Reassign chats
                 var assigned_chats = Object.assign(
@@ -109,7 +43,7 @@ async function bajaOperador(id) {
                     .map(([k, v]) => ({ [k]: v }))
                 );
                 assigned_chats.forEach((chat) => {
-                    notify_newAssig
+                    newAsign
                     .push({ id: chat.id, cont: chat.cont })
                     .on("finish", function (res) {
                         // Save the new assignment
@@ -126,3 +60,36 @@ async function bajaOperador(id) {
     );
     return baja;
 }
+
+async function recibirMensaje(id, cont) {
+  // Check if chat is already assigned
+  if (chat_asig[id]) {
+    // Push notification to operator
+    socket.recibirMensaje(chat_asig[id], id, cont);
+  } else {
+    // * We need to assign it
+    newAsign
+      .push({ cont: chat })
+      .on("finish", function (res) {
+        // Save the assignment
+        chat_asig[id] = res.op;
+        return true;
+      })
+      .on("failed", function (err) {
+        // Exception, I hope never see this
+        // ? evaluar que hacer en este caso
+        return new Error("No se puedo asignar el chat");
+      });
+  }
+}
+
+async function enviarMensaje(id, cont) {
+  messenger.enviarMensaje(id,cont);
+}
+
+
+module.exports.altaOperador = altaOperador;
+module.exports.bajaOperador = bajaOperador;
+module.exports.recibirMensaje = recibirMensaje;
+module.exports.enviarMensaje = enviarMensaje;
+module.exports.operators = operators;
