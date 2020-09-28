4b89f32455506fa1228220fa0660ee975d49b4a6
Juan M. de la Fuente <jmdelafuente22@gmail.com>
2020-09-28 13:07:57 -0300

    Cambio de nombre y exports

--- models/Chat.js
+++ models/Chat.js
@@ -1,11 +1,52 @@
-class Chat{
-    constructor (id, origen, timestamp, pendingMessage, name){
+import { URLs } from "../configs/services";
+/**
+ *
+ *
+ * @class Chat
+ */
+class Chat {
+    // * chat.id;           -> Posiblemente el mismo que usen F o W
+    // * chat.timestamp;    -> Ultimo timestamp, puede ser local o la que traiga original
+    // * chat.lastmessage;  -> Ultimo mensaje, para mantener una verificación de actualizacion
+    // * chat.origin;       -> Origen 'F' o 'W'
+    // * chat.name;         -> Nombre para mostrar / humano legible
+    // ? chat.avatar;       -> Tal vez sea necesario usar las fotos de perfil o avatares para mejor usabilidad
+
+    /**
+     *Creates an instance of Chat.
+    * @param {*} id remitente del chat, igual que el original que usan las plataformas
+    * @param {*} origen (W)hatsapp, (F)acebook...
+    * @param {*} timestamp ultima marca de tiempo
+    * @param {*} lastmessage contenido del ultmo mensaje (para que exista un chat, al menos un mensaje hubo)
+    * @param {*} name nombre humano-legible del remitente
+    * @memberof Chat
+    */
+    constructor(id, origen, timestamp, lastmessage, name) {
         this.id = id;
         this.origin = origen;
         this.timestamp = timestamp;
-        this.pendingMessage = pendingMessage;
+        this.lastmessage = lastmessage;
         this.name = name;
     }
+
+    async enviarMensaje(cont) {
+        var options = {
+            method: "POST",
+            url: URLs[this.origin],
+            headers: {
+            "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                id: this.id,
+                message: cont,
+            }),
+        };
+        request(options, function (error, response) {
+            if (error) throw new Error(error);
+        });
+    }
+
+
 }
 
 module.exports = Chat;
