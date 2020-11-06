6810aa71cbbad49abd4a9569f6453c0d3e3eb58b
Juan M. de la Fuente <jmdelafuente22@gmail.com>
2020-11-06 08:48:53 -0300

    WIP Weblogin service

---
+++ configs/weblogin.js
@@ -0,0 +1,19 @@
+const axios = require("axios").default;
+const URL = "https://weblogin.muninqn.gov.ar/api/getUserByToken/";
+const APPID = "33";
+
+module.exports.validarToken = function (token){
+    axios
+      .get(URL + token)
+      .then((usuario) => {
+        let perfil = usuario.apps.find((app) => app.id === APPID);
+        usuario.perfil = perfil;
+        return usuario;
+      })
+      .catch(function (error) {
+        throw new Error(error);
+      });
+};
+
+module.exports.URL = URL;
+module.exports.APPID = APPID;
