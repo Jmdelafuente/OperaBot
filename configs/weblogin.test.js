c958f309951e0eaad229730c649fe1ba77d96b12
Juan M. de la Fuente <jmdelafuente22@gmail.com>
2020-11-11 16:34:22 -0300

    New Feat: weblogin test

---
+++ configs/weblogin.test.js
@@ -0,0 +1,31 @@
+const assert = require("assert").strict;
+const weblogin = require("../configs/weblogin");
+const axios = require("axios").default;
+describe("integration test", function () {
+    it("deberia conectarse correctamente a weblogin y validar el TOKEN", async function () {
+    this.timeout(10000);
+    var data = JSON.stringify({ userName: "prueba2", userPass: "_Prueb@2" });
+    var config = {
+      method: "post",
+      url: "https://webLogin.muninqn.gov.ar/api/getToken",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      data: data,
+    };
+
+    let promise = axios(config)
+      .then((response) => response.data)
+      .catch(function (error) {
+        console.log(error);
+      });
+    let usuario = await promise;
+    let perfil = usuario.apps.find((app) => app.id === weblogin.APPID);
+    usuario.perfil = perfil;
+    let respuesta = await weblogin.validarToken(usuario.securityToken);
+    // Hay una clave fallada en la API, es indiferente para nuestro sistema, la ignoramos
+    delete usuario.fechaUltimoAcceso;
+    delete respuesta.fechaUltimoAcceso;
+    assert.notStrictEqual(respuesta, usuario);
+  });
+});
