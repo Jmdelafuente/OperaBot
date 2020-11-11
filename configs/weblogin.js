be940a00b249d7356156e274abac9c6a63d7e89f
Juan M. de la Fuente <jmdelafuente22@gmail.com>
2020-11-11 12:48:53 -0300

    Exports minimos y necesarios

--- configs/weblogin.js
+++ configs/weblogin.js
@@ -1,19 +1,19 @@
 const axios = require("axios").default;
 const URL = "https://weblogin.muninqn.gov.ar/api/getUserByToken/";
-const APPID = "33";
+const APPID = "40";
 
 module.exports.validarToken = function (token){
-    axios
-      .get(URL + token)
-      .then((usuario) => {
-        let perfil = usuario.apps.find((app) => app.id === APPID);
-        usuario.perfil = perfil;
-        return usuario;
-      })
-      .catch(function (error) {
-        throw new Error(error);
-      });
+  axios
+    .get(URL + token)
+    .then((usuario) => {
+      let perfil = usuario.apps.find((app) => app.id === APPID);
+      usuario.perfil = perfil;
+      return usuario;
+    })
+    .catch(function (error) {
+      throw new Error(error);
+    });
 };
 
-module.exports.URL = URL;
-module.exports.APPID = APPID;
+// module.exports.URL = URL;
+// module.exports.APPID = APPID;
