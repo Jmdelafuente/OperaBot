b699b95e7009f0ac6da9b49d68a839f61f0f9cda
Juan M. de la Fuente <jmdelafuente22@gmail.com>
2020-11-11 16:34:37 -0300

    Minor fixes: weblogin con promesa

--- configs/weblogin.js
+++ configs/weblogin.js
@@ -1,19 +1,25 @@
+const { resolve, reject } = require("bluebird");
+
 const axios = require("axios").default;
 const URL = "https://weblogin.muninqn.gov.ar/api/getUserByToken/";
 const APPID = "40";
 
-module.exports.validarToken = function (token){
-  axios
-    .get(URL + token)
-    .then((usuario) => {
-      let perfil = usuario.apps.find((app) => app.id === APPID);
-      usuario.perfil = perfil;
-      return usuario;
-    })
-    .catch(function (error) {
-      throw new Error(error);
-    });
+module.exports.validarToken = async function (token){
+    var config = {
+      method: "get",
+      url:
+        `https://weblogin.muninqn.gov.ar/api/getUserByToken/${token}`,
+    };
+    const promise = axios(config)
+      .then((response) => response.data)
+      .catch(function (error) {
+        throw new Error(error);
+      });
+  let usuario = await promise;
+  let perfil = usuario.apps.find((app) => app.id === APPID);
+  usuario.perfil = perfil;
+  return usuario;
 };
 
 // module.exports.URL = URL;
-// module.exports.APPID = APPID;
+module.exports.APPID = APPID;
