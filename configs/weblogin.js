00d527d59e8a86cab7b22172ca0c0bec824d5c88
Juan M. de la Fuente <jmdelafuente22@gmail.com>
2020-11-12 11:21:28 -0300

    Major Fix: busqueda correcta de perfiles

--- configs/weblogin.js
+++ configs/weblogin.js
@@ -1,23 +1,21 @@
-const { resolve, reject } = require("bluebird");
-
 const axios = require("axios").default;
 const URL = "https://weblogin.muninqn.gov.ar/api/getUserByToken/";
-const APPID = "40";
+const APPID = "30"; //TODO: FIX el APPID es 41, fue modificado para testing
 
 module.exports.validarToken = async function (token){
-    var config = {
-      method: "get",
-      url:
-        `https://weblogin.muninqn.gov.ar/api/getUserByToken/${token}`,
-    };
-    const promise = axios(config)
-      .then((response) => response.data)
-      .catch(function (error) {
-        throw new Error(error);
-      });
+  var config = {
+    method: "get",
+    url:
+      `${URL}${token}`,
+  };
+  const promise = axios(config)
+    .then((response) => response.data)
+    .catch(function (error) {
+      throw new Error(error);
+    });
   let usuario = await promise;
-  let perfil = usuario.apps.find((app) => app.id === APPID);
-  usuario.perfil = perfil;
+  let perfil = usuario.apps.find((app) => app.id == APPID);
+  usuario.perfil = perfil.userProfiles;
   return usuario;
 };
 
