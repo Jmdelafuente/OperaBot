00231b2f9cce0d83c2a9c333a20dd372312f3043
Juan M. de la Fuente <jmdelafuente22@gmail.com>
2020-11-12 11:21:16 -0300

    Major Fix: funcionalidad de DB completa

--- models/Operador.js
+++ models/Operador.js
@@ -4,8 +4,8 @@
  * No esta relacionado con Operator (controlador).
  * @class Operador
  */
-const db = require("../dbService");
 const weblogin = require("../configs/weblogin");
+const OperaDB = require("../dbService");
 
 class Operador {
   /**
@@ -15,101 +15,127 @@ class Operador {
    * @param {*} perfil
    * @memberof Operador
    */
-  constructor(){
+  constructor() {
     this.id = "";
-    this.idwappersona = '';
-    this.email = '';
-    this.razonSocial = '';
-    this.cuit = '';
+    this.idwappersona = "";
+    this.email = "";
+    this.razonSocial = "";
+    this.cuit = "";
     this.online = false;
     this.autoasignacion = true;
-    this.perfil = '';
+    this.perfil = "";
     this.socket = null;
-    this.db = new db("../operaBOT.db");
+    this.db = new OperaDB();
   }
 
-  init(wappersona, perfil, email, razonSocial, cuit) {
-    this.id = '';
-    this.idwappersona = wappersona;
+  init(idwappersona, perfil, email, razonSocial, cuit) {
+    this.id = "";
+    this.idwappersona = idwappersona;
     this.email = email;
     this.razonSocial = razonSocial;
     this.cuit = cuit;
-    this.online = false;
+    this.online = true;
     this.autoasignacion = true;
     this.perfil = perfil;
     this.socket = null;
+    // this.db = new db("../operaBOT.db");
+  }
+  /**
+   * Método para recuperar un operador dado un filtro o conjunto de igualdades a cumplir
+   *
+   * @param {*} filtro arreglo cuyos elementos tienen la forma [clave, valor] a buscar por igualdad.
+   * @returns {Boolean} true en caso de recuperarlo exitosamente, false en otro caso.
+   * @memberof Operador
+   */
+  async recuperar(filtro) {
+    var ret = true;
+    var promesa = this.db
+      .buscar(
+        "operadores",
+        ["email", "cuit", "razonSocial", "wapPersonaId"],
+        filtro
+      )
+      .then((response) => response[0]);
+    let operador = await promesa;
+    this.init(
+      operador.idwappersona,
+      undefined,
+      operador.email,
+      operador.razonSocial,
+      operador.cuit
+    );
+    return ret;
   }
 
   async guardar() {
-    this.db
+    var ret = false;
+    var op = this;
+    var promesa = this.db
       .insertar(
         "operadores",
-        ["email","cuit","razonSocial","wapPersonaId"],
+        ["email", "cuit", "razonSocial", "wapPersonaId"],
         [this.email, this.cuit, this.razonSocial, this.idwappersona]
       )
       .then(
         (done) => {
-          this.id = done;
-          return done;
+          op.id = done;
+          ret = done;
         },
-        (fail) => {return fail;}
+        (fail) => {
+          ret = fail;
+        }
       );
+    await promesa;
+    return ret;
   }
 
-  validar(token){
+  async validar(token) {
     var res = false;
     var operadorGuardado = null;
-    let usuario = weblogin.validarToken(token);
+    let usuario = await weblogin.validarToken(token);
     let hoy = new Date().toISOString();
     let esValido = false;
     // TODO: verificar si el usuario existe
-    if(usuario.perfil){
+    if (usuario.perfil) {
       esValido = true;
-      this.db
+      let promise = this.db
         .buscar(
           "operadores",
           ["operadorId", "email", "cuit", "razonSocial", "wapPersonaId"],
-          [["email", usuario.correoElectronico]]
-        )
-        .then(
-          (op) => {
-            if (op) {
-              operadorGuardado = op[0];
-              // TODO: revisar si es necesario actualizar datos
-              this.db.actualizar("operadores",["ultimoAcceso"],[hoy],[["id",operadorGuardado.id]]);
-            } else {
-              // TODO: No existe el usuario, pero tiene permiso, dar alta
-              operadorGuardado.id = this.db
-                .insertar(
-                  "operadores",
-                  [
-                    "email",
-                    "cuit",
-                    "razonSocial",
-                    "wapPersonaId",
-                    "ultimoAcceso",
-                  ],
-                  [
-                    usuario.correoElectronico,
-                    usuario.cuit,
-                    usuario.datosPersonales.razonSocial,
-                    usuario.datosPersonales.referenciaID,
-                    hoy,
-                  ]
-                )
-                .then((v) => {
-                  operadorGuardado.wapPersonaId =
-                    usuario.datosPersonales.referenciaID;
-                  operadorGuardado.email = usuario.correoElectronico;
-                  operadorGuardado.razonSocial =
-                    usuario.datosPersonales.razonSocial;
-                  operadorGuardado.cuit = usuario.cuit;
-                });
-            }
-          },
-          (err) => {throw new Error(err);}
+          [["email", usuario.userName]]
         );
-      if (esValido && operadorGuardado.id) {
+      let op = await promise;
+      if (op.length != 0) {
+        operadorGuardado = op[0];
+      } else {
+        // TODO: No existe el usuario, pero tiene permiso, dar alta
+        promise = this.db.insertar(
+          "operadores",
+          ["email", "cuit", "razonSocial", "wapPersonaId", "ultimoAcceso"],
+          [
+            usuario.userName,
+            usuario.datosPersonales.cuil,
+            usuario.datosPersonales.razonSocial,
+            usuario.datosPersonales.referenciaID,
+            hoy,
+          ]
+        );
+        let v = await promise;
+        operadorGuardado = {};
+        operadorGuardado.operadorId = v.id;
+        operadorGuardado.wapPersonaId = usuario.datosPersonales.referenciaID;
+        operadorGuardado.email = usuario.correoElectronico;
+        operadorGuardado.razonSocial = usuario.datosPersonales.razonSocial;
+        operadorGuardado.cuit = usuario.datosPersonales.cuil;
+      }
+      // TODO: revisar si es necesario actualizar datos
+      if (esValido && operadorGuardado.operadorId) {
+        this.db.actualizar(
+          "operadores",
+          ["ultimoAcceso"],
+          [hoy],
+          [["id", operadorGuardado.id]]
+        );
         // TODO: actualizar estado interno
         this.idwappersona = operadorGuardado.wapPersonaId;
         this.email = operadorGuardado.email;
@@ -125,6 +151,5 @@ class Operador {
     }
     return res;
   }
-
 }
 module.exports = Operador;
