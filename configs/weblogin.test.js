4ab6843616ea20d330899c5538cfc8aee495fab4
Juan M. de la Fuente <jmdelafuente22@gmail.com>
2020-11-12 07:59:53 -0300

    Minor fix: typos

--- configs/weblogin.test.js
+++ configs/weblogin.test.js
@@ -2,7 +2,7 @@ const assert = require("assert").strict;
 const weblogin = require("../configs/weblogin");
 const axios = require("axios").default;
 describe("integration test", function () {
-    it("deberia conectarse correctamente a weblogin y validar el TOKEN", async function () {
+    it("Deberia conectarse correctamente a weblogin y validar el TOKEN", async function () {
     this.timeout(10000);
     var data = JSON.stringify({ userName: "prueba2", userPass: "_Prueb@2" });
     var config = {
@@ -26,6 +26,7 @@ describe("integration test", function () {
     // Hay una clave fallada en la API, es indiferente para nuestro sistema, la ignoramos
     delete usuario.fechaUltimoAcceso;
     delete respuesta.fechaUltimoAcceso;
+    // El contenido debe ser el mismo - el objeto no necesariamente
     assert.notStrictEqual(respuesta, usuario);
   });
 });
