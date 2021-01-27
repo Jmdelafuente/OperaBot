152b20bac8d99ef75780b5bb061138d56632f642
Juan M. de la Fuente <jmdelafuente22@gmail.com>
2021-01-27 15:27:56 -0300

    Remove unused library and clean up

--- app.js
+++ app.js
@@ -6,12 +6,6 @@ const morgan = require("morgan");
 bodyParser = require("body-parser");
 jsonParser = bodyParser.json();
 app = express();
- 
-// var urlencodedParser = bodyParser.urlencoded({ extended: false });
-
-/* 
- * Set up server
- */
 app.set('port', process.env.PORT || port);
 
 // Iniciar servidor
@@ -25,13 +19,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Allowing requests from outside of the domain
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", "*"); // FIXME: update to match the domain you will make the request from
   res.header("Access-Control-Allow-Headers","Origin, X-Requested-With, Content-Type,Accept");
   next();
 });
-
-// APIs
-var api = require("./api");
 
 // Websokets (usa la configuracion previa de socket io)
 var websocket = require('./websocket.js');
