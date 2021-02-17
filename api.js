3428fb4d8e0c5a649dd97bd043bca690832cf7bf
Juan M. de la Fuente <jmdelafuente22@gmail.com>
2021-02-17 10:39:57 -0300

    New Feature: Desconexion de un usuario New Feature: Recepcion de Imagenes New Feature: Bodyparser y unificacion de origenes

--- api.js
+++ api.js
@@ -1,14 +1,38 @@
 const ms = require('./messengerService');
 const services = require('./configs/servicesConfig');
+const axios = require("axios").default;
 
 for (const [key, prefix] of Object.entries(services.PREFIXes)) {
   // * Recibimos nuevo mensaje de un messenger service
   app.post(`/api/${prefix}/newmessage`, jsonParser, (req, res) => {
+    //console.log(req.body);
     // TODO authenticate origin
-    // New whatsapp ("w") messaje
     let data = JSON.parse(req.body.body);
-    // console.log(data);
+    let type = data.type ? data.type : "chat";
     ms.nuevoMensaje(
+      data.user,
+      data.text,
+      `${key}`,
+      data.timestamp,
+      data.name,
+      type
+    ).then(
+      (cb) => {
+        res.sendStatus(200);
+      },
+      (err) => {
+        console.log(`Error en recepcion de mensaje a ${prefix} - ` + err);
+        res.sendStatus(504);
+      }
+    );
+  });
+
+  // * Recibimos nueva imagen de un messenger service
+  app.post(`/api/${prefix}/newimage`, jsonParser, (req, res) => {
+    // TODO authenticate origin
+    let data = JSON.parse(req.body.body);
+    console.log(data);
+    ms.nuevaImagen(
       data.user,
       data.text,
       `${key}`,
@@ -16,11 +40,10 @@ for (const [key, prefix] of Object.entries(services.PREFIXes)) {
       data.name
     ).then(
       (cb) => {
-        // socket.recibirMensaje("", data.user, data.text);
         res.sendStatus(200);
       },
       (err) => {
-        console.log(`Error en envio de mensaje a ${prefix} - ` + err);
+        console.log(`Error en recepcion de mensaje a ${prefix} - ` + err);
         res.sendStatus(504);
       }
     );
@@ -34,11 +57,47 @@ for (const [key, prefix] of Object.entries(services.PREFIXes)) {
         res.sendStatus(200);
       })
       .catch(() => {
-        console.log(`Error en pedido de lista de mensajes a ${prefix} - ` + err);
+        console.log(
+          `Error en pedido de lista de mensajes a ${prefix} - ` + err
+        );
+        res.sendStatus(500);
+      });
+  });
+
+  // * Desconectamos un usuario
+  app.post(`/api/${prefix}/disconnect`, jsonParser, (req, res) => {
+    // TODO authenticate origin
+    ms.disconnect(req.body, `${key}`)
+      .then(() => {
+        res.sendStatus(200);
+      })
+      .catch(() => {
+        console.log(
+          `Error en pedido de desconecion con ${prefix} - ` + err
+        );
         res.sendStatus(500);
       });
   });
 }
+
+// // * Recibimos la lista de chats de un messenger service
+//   app.post(`/api/pa/echo`, jsonParser, (req, res) => {
+//     console.log(req.body);
+//     // var data = {
+//     //     id: req.body.id,
+//     //     mensaje: `Respuesta del operador ${req.body.mensaje}`};
+//     // console.log(data);
+//     axios
+//       .post("http://b62f8f6b5b75.ngrok.io/ps/sendMessage", req.body)
+//       .then((response) => {
+//         console.log(`Respuesta de cliente: ${response.status}`);
+//         res.sendStatus(200);
+//       })
+//       .catch(function (error) {
+//         console.log(`${error}`);
+//         res.sendStatus(500);
+//       });
+//   });
 
 // app.get("/", function (req, res) {
 //   res.sendFile(path.join(__dirname + "/index.html"));
