d87f42a0a08a26d3897007faef3d6882c5a58100
Juan M. de la Fuente <jmdelafuente22@gmail.com>
2021-02-17 14:09:00 -0300

    Minor Fix: operador esta escribiendo ahora se borra

--- configs/statusConfig.js
+++ configs/statusConfig.js
@@ -1,2 +1,3 @@
 module.exports.WRITING_MESSAGE = (op) => `${op} está escribiendo`;
+module.exports.STOP_WRITING_MESSAGE = (op) => ``;
 module.exports.CHANGE_OP_MESSAGE = (op) => `Te estas comunicando con ${op}`;
