a3daddcf6f8dcd9405f0bc33503d6bd6596d0fa8
Juan M. de la Fuente <jmdelafuente22@gmail.com>
2021-02-22 15:02:41 -0300

    WIP: sending file status

--- configs/statusConfig.js
+++ configs/statusConfig.js
@@ -1,3 +1,4 @@
 module.exports.WRITING_MESSAGE = (op) => `${op} está escribiendo`;
+module.exports.SENDING_FILE = (op) => `${op} está adjuntando un archivo`;
 module.exports.STOP_WRITING_MESSAGE = (op) => ``;
 module.exports.CHANGE_OP_MESSAGE = (op) => `Te estas comunicando con ${op}`;
