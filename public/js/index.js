9b8cb32b17fa1731c17afab89da22af52ce68bed
Juan M. de la Fuente <jmdelafuente22@gmail.com>
2021-02-22 14:09:25 -0300

    Minor fix: stickers working

--- public/js/index.js
+++ public/js/index.js
@@ -134,6 +134,7 @@ $(function () {
         case "chat": // * Emoji o chat
           msj.innerHTML = twemoji.parse(cont);
           break;
+        case "sticker": // ! Sticker
         case "image": // * Foto
           let link = document.createElement("a");
           let canvas = document.createElement("canvas");
@@ -153,10 +154,9 @@ $(function () {
           msj.appendChild(link);
           link.appendChild(canvas);
           break;
-        case "sticker": // ! Sticker
-        case "ptt":     //! AUDIO
+        case "ptt": //! AUDIO
           let audio = document.createElement("audio");
-          audio.setAttribute('controls','');
+          audio.setAttribute("controls", "");
           audio.src = cont;
           msj.appendChild(audio);
           break;
