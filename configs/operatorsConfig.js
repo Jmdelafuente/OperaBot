379d962fd4d6d9e598840def771c14b854103e2b
Juan M. de la Fuente <jmdelafuente22@gmail.com>
2021-02-24 09:41:35 -0300

    New feature: config para automessage de working hours

--- configs/operatorsConfig.js
+++ configs/operatorsConfig.js
@@ -1,2 +1,4 @@
 module.exports.START_TIME = "08:00";
 module.exports.END_TIME = "20:00";
+module.exports.AUTOMESSAGE = "En este momento nuestros operadores no están disponibles. El horario de atención es de 8hs a 20hs";
+module.exports.USE_AUTOMESSAGE = true;
