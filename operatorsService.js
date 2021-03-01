72c34dff0ebca1a203d93827b3103b3c09062d03
Kuro933 <f3d3x93@gmail.com>
2021-03-01 10:08:22 -0300

    minor fix: nombre de operador en ESCRIBIENDO

--- operatorsService.js
+++ operatorsService.js
@@ -303,7 +303,7 @@ async function confirmarVisto(chatId, channelId) {
 async function escribiendo(chatID, channelID, isWriting = true) {
   let operador = operators[operators_channels[channelID]];
   let str = operador.razonSocial;
-  let index = str.indexOf(",") + 3;
+  let index = str.indexOf(",") + 2;
   let nombre = str.substring(index);
   if (isWriting) {
     messenger.enviarEstado(
