49ad50dc85a249253c6797c6e2eaed5402bdcec0
Kuro933 <f3d3x93@gmail.com>
2021-03-16 09:14:25 -0300

    minor fix: cambio de opcionesAdmin

--- websocket.js
+++ websocket.js
@@ -39,7 +39,7 @@ appFront.set("port", portFront);
 
 appFront.use(express.static(path.join(__dirname, "public")));
 
-appFront.get("/", function (req, res) {
+appFront.get("/operadores", function (req, res) {
   console.log("entre al get");
   let param = req.query.SESSIONKEY;
   let perfil = op.validar(param);
