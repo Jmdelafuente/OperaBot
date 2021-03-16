cd7d113f38b992a8b10aee2e90815d576cdaad6e
Kuro933 <f3d3x93@gmail.com>
2021-03-16 09:33:10 -0300

    minor fix: cambio en el path de index.html

--- websocket.js
+++ websocket.js
@@ -40,14 +40,14 @@ appFront.set("port", portFront);
 appFront.use(express.static(path.join(__dirname, "public")));
 
 appFront.get("/operadores", function (req, res) {
-  console.log("entre al get");
+  console.log(__dirname);
   let param = req.query.SESSIONKEY;
   let perfil = op.validar(param);
   if(perfil != -1){
     if(perfil == 3){
       res.sendFile(__dirname + "/public/admin/index.html");
     }else{
-      res.sendFile(__dirname + "/public/admin/index.html");
+      res.sendFile(__dirname + "/public/index.html");
     }
   }else{
     res.send(Error("Operador no valido"))
