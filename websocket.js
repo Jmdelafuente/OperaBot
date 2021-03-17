50b8dc89bd15da910f56151b4439719207db971b
Kuro933 <f3d3x93@gmail.com>
2021-03-17 09:20:35 -0300

    minor fix:url

--- websocket.js
+++ websocket.js
@@ -1,7 +1,7 @@
 // Imports from websockets
 const express = require("express");
 var appFront = express();
-var cors = require("cors");
+//var cors = require("cors");
 var helmet = require("helmet");
 var http = require("http").Server(appFront);
 var io = require("socket.io")(http, {
@@ -26,9 +26,9 @@ const { chatsList } = require("./messengerService");
 
 // Front for websockets
 appFront.set("port", portFront);
-appFront.use(helmet());
+//appFront.use(helmet());
 
-appFront.use(cors);
+/*appFront.use(cors);
  appFront.use(function (req, res, next) {
    res.header("Access-Control-Allow-Origin", "*"); // FIXME: update to match the domain you will make the request from
    res.header(
@@ -36,7 +36,7 @@ appFront.use(cors);
      "Origin, X-Requested-With, Content-Type,Accept"
    );
    next();
- });
+ });*/
 
 appFront.use(express.static(path.join(__dirname, "public")));
 
