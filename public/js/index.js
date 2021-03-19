5cc3fd4f7e804db741e0d6d28a884311140cf734
Kuro933 <f3d3x93@gmail.com>
2021-03-19 09:30:44 -0300

    minor fix: operadores

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,6 @@
 //const SURL = "128.53.1.23";
-//const SURL = "chat.muninqn.gov.ar/operadores/";
-const SURL = "localhost";
+const SURL = "chat.muninqn.gov.ar/operadores/";
+//const SURL = "localhost";
 const socket = io(`${SURL}:2999`);
 var blueprints={}; 
 var conn = false;
