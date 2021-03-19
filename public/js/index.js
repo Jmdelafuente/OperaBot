ca498bf921ba4dc6a8c7c769ee59a84b5b134426
Kuro933 <f3d3x93@gmail.com>
2021-03-19 12:18:10 -0300

    minor fix: operadores

--- public/js/index.js
+++ public/js/index.js
@@ -1,9 +1,8 @@
-import { io } from "socket.io-client";
 //const SURL = "128.53.1.23";
 //const SURL = "chat.muninqn.gov.ar/operadores/";
 //const SURL = "localhost";
 
-const socket = io('https://chat.muninqn.gov.ar/operadores/', {path: '/socket.io'});
+const socket = io('https://chat.muninqn.gov.ar', {path: '/socket.io'});
 var blueprints = {};
 var conn = false;
 var chatListAll = [];
