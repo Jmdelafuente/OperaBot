bffb2e5cf4462dddf374fa2e88725e822b696f5b
Kuro933 <f3d3x93@gmail.com>
2021-03-19 11:55:33 -0300

    minor fix: operadores

--- websocket.js
+++ websocket.js
@@ -60,7 +60,7 @@ http.listen(portFront);
 
 // * EVENTOS * //
 
-io.of('/operadores/')
+
 io.on("connection", function (socket) {
   console.log("se conecto un operador");
   socket.on("send_op_message", function (msg) {
