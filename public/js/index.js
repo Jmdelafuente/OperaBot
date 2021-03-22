d314ec3439ca33ada4de3b4eafd36fbf6d6ce2bc
Kuro933 <f3d3x93@gmail.com>
2021-03-22 10:22:03 -0300

    minor fix:url con https

--- public/js/index.js
+++ public/js/index.js
@@ -1,8 +1,8 @@
-//const SURL = "128.53.1.23";
+const SURL = "https://128.53.1.23/operadores/";
 //const SURL = "chat.muninqn.gov.ar/operadores/";
-const SURL = "http://localhost";
+//const SURL = "http://localhost";
 
-const socket = io(`${SURL}:2999`);
+const socket = io(`${SURL}`);
 var blueprints = {};
 var conn = false;
 var chatListAll = [];
