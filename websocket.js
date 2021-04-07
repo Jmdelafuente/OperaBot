57b9de9b369efb1f335c275447f8b8d6a5e854fd
Kuro933 <f3d3x93@gmail.com>
2021-04-07 12:01:39 -0300

    minor fix: nombre en lugar de id op

--- websocket.js
+++ websocket.js
@@ -292,6 +292,8 @@ const mensajesByChat = function(id, listamensajes, socket, append=false) {
       (nombre) => {
         let split = nombre.split(",");
         element.operador_id = split[1]
+        console.log(split[1]);
+        console.log(element.operador_id);
       },
       (error) => {
         //  TODO: registrar el error
