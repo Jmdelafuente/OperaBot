b819472af9171fbb6d4bc6958bc5cd097431b8df
Kuro933 <f3d3x93@gmail.com>
2021-04-07 10:29:26 -0300

    wip: traer id op

--- websocket.js
+++ websocket.js
@@ -284,7 +284,7 @@ async function quieremail(operador, idUser, chat) {
 const mensajesByChat = function(id, listamensajes, socket, append=false) {
   let msg = {};
   msg.id = id;
-  console.log(`che la lista en realidad es todo esto ${listamensajes[1].operador_id}`);
+  console.log(`che la lista en realidad es todo esto ${listamensajes[1].contenido}`);
   msg.lista = listamensajes;
   if(append){
     socket.emit("getMoreMessagesByChat", msg);
