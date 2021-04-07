34accdfae95c21e66554683261bce5a6bef230de
Kuro933 <f3d3x93@gmail.com>
2021-04-07 08:37:22 -0300

    minor fix: mostrar que operador respondio

--- operatorsService.js
+++ operatorsService.js
@@ -333,7 +333,7 @@ async function obtenerNombre(operadorId) {
   ).then(
     (nombre) => {
       razonOperador = JSON.stringify(nombre);
-      console.log(`el nombre es ${razonOperador}`);
+      console.log(`el nombre es ${razonOperador[0].razonSocial}`);
     },
     (error) => {
       console.error(error);
