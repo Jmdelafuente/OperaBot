03b7746b1f1370ac52c58f986adca1d9adb98f12
Kuro933 <f3d3x93@gmail.com>
2021-04-16 09:46:34 -0300

    minor fix: connect admin

--- public/admin/websocketAdmin.js
+++ public/admin/websocketAdmin.js
@@ -45,6 +45,21 @@ app.set("port", portFront);
 
 app.use(express.static(path.join(__dirname, "public")));
 
+app.get("/operadores/admin/", function (req, res) {
+  console.log("entre al get");
+  let param = req.query.SESSIONKEY;
+  let perfil = op.validar(param);
+  if(perfil != -1){
+    if(perfil == 3){
+      res.sendFile(__dirname + "public/admin/index.html");
+    }else{
+      res.sendFile(__dirname + "index.html");
+    }
+  }else{
+    res.send(Error("Operador no valido"))
+  }
+});
+
 http.listen(app.get('port'), () => {
     console.log('server on port', app.get('port'));
 });
