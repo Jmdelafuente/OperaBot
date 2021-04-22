f85878182f1d846263d25c59c38a33f0e06ed6bc
Kuro933 <f3d3x93@gmail.com>
2021-04-22 10:28:48 -0300

    minor fix: list.sort

--- public/js/index.js
+++ public/js/index.js
@@ -512,7 +512,9 @@ $(function () {
       `newList: ${asig} - All: ${chatListAll} - Asign: ${chatListAsign}`
     );
     console.log(`la lista es ${lista}`);
-    //lista.sort(function (a , b) {return b.timestamp - a.timestamp});
+    if(lista.length > 1){
+      lista.sort(function (a , b) {return b.timestamp - a.timestamp});
+    }
     console.log(`la lista ordenada es ${lista}`);
     for (let c of Object.keys(lista)) {
       if (asig) {
