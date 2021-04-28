af4181db67e06f0ec05f1cba9085d96fb6f9c1bb
Kuro933 <f3d3x93@gmail.com>
2021-04-28 12:28:42 -0300

    wip: que la parte visual corresponda a la funcional

--- operatorsService.js
+++ operatorsService.js
@@ -370,9 +370,12 @@ async function obtenerNombre(operadorId) {
 async function confirmarVisto(chatId, channelId) {
   // let operador = operators[operadorId];
   // Al abrir el mensaje, la asignacion pasa a ser estable (no se busca nuevo operador para el chat)
-  console.log(`en confirmar visto, antes de asignacion nueva ${chat_asig[chatId].operadorId} y ${chat_asig[chatId].chatId}`);
-  if (chat_asig[chatId].operadorId != operators_channels[channelId]){
-    console.log("este chat no era tuyo, pero te lo vas a robar");
+  if(chat_asig[chatId]){
+    console.log(`en confirmar visto, antes de asignacion nueva ${chat_asig[chatId].operadorId} y ${chat_asig[chatId].chatId}`);
+    if (chat_asig[chatId].operadorId != operators_channels[channelId]){
+      console.log("este chat no era tuyo, pero te lo vas a robar");
+      console.log(`puedo obtener socket? ${operators[chat_asig[chatId].operadorId].socket}`);
+    }
   }
   let asignacion = new Asignacion(chatId, operators_channels[channelId]);
   await asignacion.guardar();
