c72193a7d13bb215e1a2bdef6b32fbff5487a919
Kuro933 <f3d3x93@gmail.com>
2021-04-28 11:43:26 -0300

    wip: que la parte visual corresponda a la funcional

--- operatorsService.js
+++ operatorsService.js
@@ -370,6 +370,7 @@ async function obtenerNombre(operadorId) {
 async function confirmarVisto(chatId, channelId) {
   // let operador = operators[operadorId];
   // Al abrir el mensaje, la asignacion pasa a ser estable (no se busca nuevo operador para el chat)
+  console.log(`en confirmar visto, antes de asignacion nueva ${chat_asig[chatId].operadorId} y ${chat_asig[chatId].chatId}`);
   let asignacion = new Asignacion(chatId, operators_channels[channelId]);
   await asignacion.guardar();
   var asignado = asignacion.asignacionEstable;
