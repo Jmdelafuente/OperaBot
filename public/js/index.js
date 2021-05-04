d8d19b2dca5516b72d5b877270f8d9462d642b37
Kuro933 <f3d3x93@gmail.com>
2021-05-04 12:16:41 -0300

    minor fix: ojala funcione y no se ponga en el otro chat

--- public/js/index.js
+++ public/js/index.js
@@ -3,6 +3,7 @@ const SURL = "128.53.80.105";
 const socket = io(`${SURL}`, { 'forceNew': true, path: '/operadores/socket.io' });
 //const SURL = "https://chat.muninqn.gov.ar";
 //const socket = io(`${SURL}` , {'forceNew': true, path:'/operadores/socket.io'});
+
 var blueprints = {};
 var conn = false;
 var chatListAll = [];
