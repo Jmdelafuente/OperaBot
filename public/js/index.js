e19791d1fc02866937dc424d5e0159ffa993a895
Kuro933 <f3d3x93@gmail.com>
2021-05-14 10:12:26 -0300

    minor fix: asignados no dibujan

--- public/js/index.js
+++ public/js/index.js
@@ -731,6 +731,8 @@ $(function () {
  socket.on("send_op_list", function (listaChats) {
     // let listaChats = JSON.parse(msg);
     console.log(JSON.stringify(listaChats));
+    console.log(listaChats.chats);
+    console.log(listaChats.chats[key]);
     newList(listaChats.chats, listaChats.asignado);
   });
   socket.on("recive_op_message", function (msg) {
